62d311b922732f045afbb6894b27e1f515a99c72
Tan <tanxin007@aliyun.com>
2018-05-09 23:36:14 +0800

    edit 2012 test5

--- 2012/test5.c
+++ 2012/test5.c
@@ -6,18 +6,36 @@
  * （如“aaaaa”与"bbb”的合并结果为“abababaa”;而“bbb”与“aaaaa”的合并结果为“bababaaa”）,并将结果存入文件a.txt中。
  */
 
+#include <mem.h>
 #include "stdio.h"
 
+
 int main() {
-    FILE *filea = fopen("a.txt", "wr");;
-    FILE *fileb = fopen("b.txt", "w");;
+    FILE *filea = fopen("a.txt", "r");  // a.txt  b.txt 自行创建
+    FILE *fileb = fopen("b.txt", "r");
 
     if (filea == NULL || fileb == NULL) {
+        fclose(filea);
+        fclose(fileb);
         return 0;
     }
+    int index = 0;
+    char ch[40] = "";
 
-    char ch = NULL;
-
+    while (1) {
+        ch[index++] = (char) fgetc(filea);
+        ch[index++] = (char) fgetc(fileb);
+        if (ch[index - 1] == -1 && ch[index - 2] == -1)
+            break;
+    }
+    FILE *fp = fopen("a.txt", "w");
+    for (int i = 0; i < strlen(ch); i++) {
+        if (ch[i] > 0)
+            fputc(ch[i], fp);
+    }
+    fclose(fp);
+    fclose(filea);
+    fclose(fileb);
 
     return 1;
 }
